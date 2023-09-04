7bab2766de788e064b9d15c2829692056f7c3d9c
Peyha Nicole <peyha.nicole@outlook.fr>
2023-09-04 23:37:36 +0200

    feat(arithmetic): implement extended euclidean algorithm

--- arithmetic/arithmetic/src/lib.rs
+++ arithmetic/arithmetic/src/lib.rs
@@ -12,6 +12,18 @@ pub fn gcd(a: u64, b: u64) -> u64{
     gcd(b, a % b)
 }
 
+pub fn extended_gcd(a: u64, b: u64) -> (i64, i64, i64){
+    if a < b{
+        let (d, u, v): (i64, i64, i64) = extended_gcd(b, a);
+        return (d, v, u);
+    }
+    if b == 0{
+        return (a as i64, 1, 0);
+    }
+    let (d, u, v): (i64, i64, i64) = extended_gcd(b, a % b);
+    return (d, v, u-(a as i64)/(b as i64)*v);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +35,12 @@ mod tests {
 
         let result = gcd(25, 5);
         assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn extended_gcd_test(){
+        let (res_d, res_u, res_v) = extended_gcd(42, 150);
+        let potential_d = 42 * res_u + 150 * res_v;
+        assert_eq!(potential_d, res_d);
     }
 }
